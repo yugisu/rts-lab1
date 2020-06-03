b739724d7b9d76b81cdac82e9b112b9ec4aa64ca
Dmytrii Puzyr <slymahel@gmail.com>
2020-06-03 11:25:38 +0300

    Add Rxx and Rxy calculation speed comparison in lab1.2

--- autocorrelation_12/README.md
+++ autocorrelation_12/README.md
@@ -1,5 +1,12 @@
 # Lab1.1
 
+## Rxx vs Rxy execution time comparison
+
+```
+(venv) D:\Study\University\.VI\RTS\labs\lab1>python -m autocorrelation_12
+Rxx time: 0.00032080000000000997, Rxy time: 0.00032599999999993745
+```
+
 ## Example
 
 ![Execution example](docs/execution-example.png)
--- autocorrelation_12/__main__.py
+++ autocorrelation_12/__main__.py
@@ -1,4 +1,5 @@
 if __name__ == '__main__':
+    import time
     import numpy as np
     import matplotlib.pyplot as plt
 
@@ -12,8 +13,16 @@ if __name__ == '__main__':
     sig_x = np.array([x_gen(lag) for lag in DISCRETE_COUNTS])
     sig_y = np.array([y_gen(lag) for lag in DISCRETE_COUNTS])
 
+
+    time_Rxx_start = time.perf_counter()
     Rxx = autocorr(sig_x, sig_x)
+    time_Rxx = time.perf_counter() - time_Rxx_start
+
+    time_Rxy_start = time.perf_counter()
     Rxy = autocorr(sig_x, sig_y)
+    time_Rxy = time.perf_counter() - time_Rxy_start
+
+    print(f"Rxx time: {time_Rxx}, Rxy time: {time_Rxy}")
 
     fig, axs = plt.subplots(3, 1)
 
